4e3a51b01c4447aec7785e7a0c430e4a63af22f8
NexJuninho <ronaldojunior.dvp@outlook.com>
2026-05-18 20:14:12 -0300

    criado lista de candidatos

---
+++ main.js
@@ -0,0 +1,19 @@
+const candidato = [
+    {nome: "João",
+    area: "Desenvolvimento de Software",
+    habilidades: ["JavaScript", "Python", "Java"],
+    experienciaMeses: 5,
+    }
+
+    {nome: "Felipe",
+    area: "Front-end",
+    habilidades: ["HTML", "CSS", "JavaScript"],
+    experienciaMeses: 24,
+    }
+
+    {nome: "Manuela",
+    area: "Back-end",
+    habilidades: ["Python", "Django", "SQL", "Java"],
+    experienciaMeses: 15,
+    }
+]
